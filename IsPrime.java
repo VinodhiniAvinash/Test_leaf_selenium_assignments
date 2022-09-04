fd2fd790d3537e861323767e77dfd1b9143dfecc
VinodhiniAvinash <112798583+VinodhiniAvinash@users.noreply.github.com>
2022-09-04 15:03:00 +0530

    Add files via upload

--- IsPrime.java
+++ IsPrime.java
@@ -4,16 +4,16 @@ public class IsPrime {
 
 	public static void main(String[] args) {
 	
-		int n=7,count=0;
+		int n=13,count=0;
 		for (int i=2;i<=n;i++)
 		{
 			if(n%i==0)
 				count++;
 		}
 			if (count>0)
-				System.out.println(n + " is a prime number");
+				System.out.println(n + " is not a prime number");
 			else 
-				System.out.println( n + "is not a prime number");
+				System.out.println( n + "is a prime number");
 
 	}
 
