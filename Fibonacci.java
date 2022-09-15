ca308c388562177307f279c52e4bc3549ade0cf2
VinodhiniAvinash <112798583+VinodhiniAvinash@users.noreply.github.com>
2022-09-15 20:20:54 +0530

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,24 @@
+package Week1.Day1;
+
+public class Fibonacci {
+
+	public static void main(String[] args) {
+		int num1=0,num2=1,sum=0,n=11;
+		
+		System.out.println("The fibonacci series are");
+		
+		System.out.println(num1);
+		System.out.println(num2);
+		
+		for(int i=1;i<n;i++) {
+			
+			sum = num1 + num2;
+			num1 = num2;
+			num2 = sum;
+			System.out.println(sum);
+		}
+			
+
+	}
+
+}
