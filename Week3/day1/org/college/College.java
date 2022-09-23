412830724d3646f725eff8aa5d437fd6ab9366f5
VinodhiniAvinash <112798583+VinodhiniAvinash@users.noreply.github.com>
2022-09-23 22:31:13 +0530

    Add files via upload

---
+++ Week3/day1/org/college/College.java
@@ -0,0 +1,17 @@
+package org.college;
+
+public class College {
+
+	public static void collegeName() {
+		System.out.println("College Name : XYZ college");
+	}
+	
+	public static void collegeCode() {
+		System.out.println("College Code : 1543");
+	}
+	
+	public static void collegeRank() {
+		System.out.println("College Rank : 1");
+	}
+
+}
---
+++ Week3/day1/org/department/Department.java
@@ -0,0 +1,11 @@
+package org.department;
+
+import org.college.College;
+
+public class Department extends College{
+	
+	public static void deptName() {
+		System.out.println("Department Name : ECE");
+	}
+
+}
